e652f6db55d7dc86b56261574a4adfa163f1ad83
Barry Tikalsky <tikal004@umn.edu>
2023-09-28 16:19:30 -0500

    streamilined validation of json path

--- src/utilities.py
+++ src/utilities.py
@@ -192,45 +192,18 @@ def run_stage(stage_name, j_args, logger):
         logger.error(f"Error running {stage_name}")
         return False
 
-def valid_readable_file(path):
-    """
-    Throw exception unless parameter is a valid readable filepath string. Use
-    this, not argparse.FileType("r") which leaves an open file handle.
-    :param path: Parameter to check if it represents a valid filepath
-    :return: String representing a valid filepath
-    """
-    return validate(path, lambda x: os.access(x, os.R_OK),
-                    os.path.abspath, "Cannot read file at '{}'")
-
-
 def valid_readable_json(path):
     """
     :param path: Parameter to check if it represents a valid .json file path
     :return: String representing a valid .json file path
     """
-    return validate(path, lambda _: os.path.splitext(path)[-1] == ".json",
-                    valid_readable_file,
-                    "'{}' is not a path to a readable .json file")
-
-def validate(to_validate, is_real, make_valid, err_msg, prepare=None):
-    """
-    Parent/base function used by different type validation functions. Raises an
-    argparse.ArgumentTypeError if the input object is somehow invalid.
-    :param to_validate: String to check if it represents a valid object 
-    :param is_real: Function which returns true iff to_validate is real
-    :param make_valid: Function which returns a fully validated object
-    :param err_msg: String to show to user to tell them what is invalid
-    :param prepare: Function to run before validation
-    :return: to_validate, but fully validated
-    """
     try:
-        if prepare:
-            prepare(to_validate)
-        assert is_real(to_validate)
-        return make_valid(to_validate)
+        assert os.access(path, os.R_OK)
+        assert os.path.splitext(path)[-1] == ".json"
+        return path
     except (OSError, TypeError, AssertionError, ValueError,
             argparse.ArgumentTypeError):
-        raise argparse.ArgumentTypeError(err_msg.format(to_validate))
+        raise argparse.ArgumentTypeError(f"{path} is not a path to a readable .json file")
 
 def validate_parameter_json(j_args, json_path, logger):
     """
