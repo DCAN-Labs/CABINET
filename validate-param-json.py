4879fc514dcab32d28e9c65046d3286ed2a6e9a4
Barry Tikalsky <tikal004@umn.edu>
2023-09-29 09:51:09 -0500

    fixed imports

--- validate-param-json.py
+++ validate-param-json.py
@@ -4,19 +4,13 @@
 """
 Script for validating cabinet input parameter jsons without running the whole pipeline
 """
-
-from src.utilities import (
-    make_logger,
-    get_args,
-    validate_parameter_json
-)
+from src.utilities import get_args
+from src.validate import validate_parameter_json
 
 def main():
-    logger = make_logger()  # Make object to log error/warning/status messages
-
     # Get and validate command-line arguments and parameters from .JSON file
     args = get_args()
-    validate_parameter_json(args.parameter_json, logger)
+    validate_parameter_json(args.parameter_json)
 
 
 if __name__ == "__main__":
