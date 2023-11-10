7381cdddba080b4281c89826b07d2ed7b950f98c
Barry Tikalsky <tikal004@umn.edu>
2023-11-10 14:25:47 -0600

    adding basic tests

---
+++ lolcow_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from src.valid_jargs_class import ValidJargs
+from src.wrapper_class import Wrapper
+
+param_json = "lolcow_test.json"
+
+Jargs = ValidJargs(param_json)
+
+Test = Wrapper(Jargs)
+
+def test_run_stage_pass():
+
+    assert Test.run_stage("lolcow") == True
+
+def test_run_stage_fail():
+    
+    assert Test.run_stage("dockerfail") == False
